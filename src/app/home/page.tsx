7d0481aab14985edcecbec77ee9b7d71f34bfa3d
jvdalaglio <jvdalaglio@gmail.com>
2024-11-16 18:14:15 -0300

    ajuste filtros

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -240,7 +240,7 @@ export default function Home() {
                   icon={AccountBalanceWalletIcon}
                 ></Card>
               </div>
-              <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 p-2">
+              <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 p-2 w-full">
                 <div className="grid">
                   <label htmlFor="date">Data</label>
                   <input
